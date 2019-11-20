016c7e738a7ba68ea538b6a765dbe8e5c18dc074
zwping <1101558280@qq.com>
2019-11-20 16:44:57 +0800

    1

--- app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
@@ -54,9 +54,13 @@ import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 
 import static android.widget.Toast.makeText;
 
+/**
+ * 官方文档 https://cmusphinx.github.io/wiki/tutorialandroid/
+ */
 public class PocketSphinxActivity extends Activity implements
         RecognitionListener {
 
+    /* 命名搜索允许快速重新配置解码器 */
     /* Named searches allow to quickly reconfigure the decoder */
     private static final String KWS_SEARCH = "wakeup";
     private static final String FORECAST_SEARCH = "forecast";
@@ -65,7 +69,8 @@ public class PocketSphinxActivity extends Activity implements
     private static final String MENU_SEARCH = "menu";
 
     /* Keyword we are looking for to activate menu */
-    private static final String KEYPHRASE = "hello";
+//    private static final String KEYPHRASE = "oh mighty computer";
+    private static final String KEYPHRASE = "he computer";
 
     /* Used to handle permission request */
     private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
@@ -85,8 +90,7 @@ public class PocketSphinxActivity extends Activity implements
         captions.put(PHONE_SEARCH, R.string.phone_caption);
         captions.put(FORECAST_SEARCH, R.string.forecast_caption);
         setContentView(R.layout.main);
-        ((TextView) findViewById(R.id.caption_text))
-                .setText("Preparing the recognizer");
+        ((TextView) findViewById(R.id.caption_text)).setText("正在准备语音解码器");
 
         // Check if user has given permission to record audio
         int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
@@ -99,11 +103,14 @@ public class PocketSphinxActivity extends Activity implements
         new SetupTask(this).execute();
     }
 
+    /*** 异步加载解码器 ***/
     private static class SetupTask extends AsyncTask<Void, Void, Exception> {
         WeakReference<PocketSphinxActivity> activityReference;
+
         SetupTask(PocketSphinxActivity activity) {
             this.activityReference = new WeakReference<>(activity);
         }
+
         @Override
         protected Exception doInBackground(Void... params) {
             try {
@@ -115,6 +122,7 @@ public class PocketSphinxActivity extends Activity implements
             }
             return null;
         }
+
         @Override
         protected void onPostExecute(Exception result) {
             if (result != null) {
@@ -128,9 +136,8 @@ public class PocketSphinxActivity extends Activity implements
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions, @NonNull  int[] grantResults) {
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
         if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Recognizer initialization is a time-consuming and it involves IO,
@@ -145,7 +152,6 @@ public class PocketSphinxActivity extends Activity implements
     @Override
     public void onDestroy() {
         super.onDestroy();
-
         if (recognizer != null) {
             recognizer.cancel();
             recognizer.shutdown();
@@ -161,8 +167,8 @@ public class PocketSphinxActivity extends Activity implements
     public void onPartialResult(Hypothesis hypothesis) {
         if (hypothesis == null)
             return;
-
         String text = hypothesis.getHypstr();
+        System.out.println("lis ------ 部分结果" + text);
         if (text.equals(KEYPHRASE))
             switchSearch(MENU_SEARCH);
         else if (text.equals(DIGITS_SEARCH))
@@ -183,12 +189,14 @@ public class PocketSphinxActivity extends Activity implements
         ((TextView) findViewById(R.id.result_text)).setText("");
         if (hypothesis != null) {
             String text = hypothesis.getHypstr();
+            System.out.println("lis ------ 结果" + text);
             makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public void onBeginningOfSpeech() {
+//        System.out.println("lis ------ asr start");
     }
 
     /**
@@ -196,13 +204,15 @@ public class PocketSphinxActivity extends Activity implements
      */
     @Override
     public void onEndOfSpeech() {
+//        System.out.println("lis ------ asr end");
         if (!recognizer.getSearchName().equals(KWS_SEARCH))
             switchSearch(KWS_SEARCH);
     }
 
+    /*** 切换搜索关键词??? ***/
     private void switchSearch(String searchName) {
+        System.out.println("lis ---- switch  " + searchName);
         recognizer.stop();
-
         // If we are not spotting, start listening with timeout (10000 ms or 10 seconds).
         if (searchName.equals(KWS_SEARCH))
             recognizer.startListening(searchName);
@@ -216,47 +226,49 @@ public class PocketSphinxActivity extends Activity implements
     private void setupRecognizer(File assetsDir) throws IOException {
         // The recognizer can be configured to perform multiple searches
         // of different kind and switch between them
-
+        //识别器可以配置为执行多次搜索
+        //不同种类并在它们之间切换
         recognizer = SpeechRecognizerSetup.defaultSetup()
-                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
-                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
-
-                .setRawLogDir(assetsDir) // To disable logging of raw audio comment out this call (takes a lot of space on the device)
-
+                .setAcousticModel(new File(assetsDir, "en-us-ptm")) // 声学模型
+                .setDictionary(new File(assetsDir, "cmudict-en-us.dict")) // 字典
+                .setRawLogDir(assetsDir) // 日志目录 To disable logging of raw audio comment out this call (takes a lot of space on the device)
                 .getRecognizer();
         recognizer.addListener(this);
-
+        /*在您的应用程序中，您可能不需要添加所有这些搜索。
+           将它们添加到此处进行演示。 您只能留下一个。          * /
         /* In your application you might not need to add all those searches.
           They are added here for demonstration. You can leave just one.
          */
 
-        // Create keyword-activation search.
+        // Create keyword-activation search. 创建关键字激活搜索
         recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
 
-        // Create grammar-based search for selection between demos
+        // Create grammar-based search for selection between demos 创建基于语法的搜索以在演示之间进行选择
         File menuGrammar = new File(assetsDir, "menu.gram");
         recognizer.addGrammarSearch(MENU_SEARCH, menuGrammar);
 
-        // Create grammar-based search for digit recognition
+        // Create grammar-based search for digit recognition 创建基于语法的数字识别搜索
         File digitsGrammar = new File(assetsDir, "digits.gram");
         recognizer.addGrammarSearch(DIGITS_SEARCH, digitsGrammar);
 
-        // Create language model search
+        // Create language model search 创建语言模型搜索
         File languageModel = new File(assetsDir, "weather.dmp");
         recognizer.addNgramSearch(FORECAST_SEARCH, languageModel);
 
-        // Phonetic search
+        // Phonetic search 语音搜索
         File phoneticModel = new File(assetsDir, "en-phone.dmp");
         recognizer.addAllphoneSearch(PHONE_SEARCH, phoneticModel);
     }
 
     @Override
     public void onError(Exception error) {
+        System.out.println("lis ------ asr error" + error.getMessage());
         ((TextView) findViewById(R.id.caption_text)).setText(error.getMessage());
     }
 
     @Override
     public void onTimeout() {
+        System.out.println("lis ------ asr timeout");
         switchSearch(KWS_SEARCH);
     }
 }
